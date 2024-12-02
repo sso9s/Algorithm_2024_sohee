e64407925aa8d578a057e74d485e929cbcc164d0
sso9s <114461598+sso9s@users.noreply.github.com>
2024-12-02 15:53:47 +0900

    Update Program.cs

--- 013_chained/Program.cs
+++ 013_chained/Program.cs
@@ -11,7 +11,7 @@ namespace _013_chained
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
             int[] arr = { 10, 30, 5, 60, 15, 10 };
             int n = 6;
@@ -21,31 +21,31 @@ namespace _013_chained
 
         private static void matrixChainOrder(int[] p, int n)
         {
-            int[,] m = new int[n, n]; // 곱셈 횟수
+            int[,] m = new int[n, n]; // 곱셈횟수
             int[,] s = new int[n, n]; // 최적분할 위치
 
             // L은 곱할 행렬의 개수
             for (int L = 2; L < n; L++)
             {
-                for (int i = 1; i <= n-L; i++)
+                for (int i = 1; i <= n - L; i++)
                 {
                     int j = i + L - 1;
-                    m[i,j] = int.MaxValue;
-                    for (int k = 1; k < j; k++)
+                    m[i, j] = int.MaxValue;
+                    for (int k = i; k < j; k++)
                     {
                         int q = m[i, k] + m[k + 1, j] + p[i - 1] * p[k] * p[j];
-                        if(q < m[i,j])
+                        if (q < m[i, j])
                         {
-                            m[i,j] = q;
-                            s[i,j] = k;
+                            m[i, j] = q;
+                            s[i, j] = k;
                         }
-                    }    
+                    }
                 }
                 Console.WriteLine("L = {0}", L);
-                printArrary(m, n);
-                printArrary(s, n);
+                printArray(m, n);
+                printArray(s, n);
             }
-            // 최적 곱셈 순서(재귀함수)
+            //최적 곱셈 순서(재귀함수)
             printParen(s, 1, n - 1);
             Console.WriteLine();
         }
@@ -57,17 +57,20 @@ namespace _013_chained
             else
             {
                 Console.Write("(");
-                printParen(s, i, s[i,j]);
-                printParen(s, s[i,j] + 1, j);
-                Console.WriteLine(")");
+                printParen(s, i, s[i, j]);
+                printParen(s, s[i, j] + 1, j);
+                Console.Write(")");
             }
         }
 
-        private static void printArrary(int[,] m, int n)
+        private static void printArray(int[,] m, int n)
         {
             for (int i = 1; i < n; i++)
+            {
                 for (int j = 1; j < n; j++)
-                    Console.Write("{0:8}", m[i, j]);
+                    Console.Write("{0,8}", m[i, j]);
+                Console.WriteLine();
+            }
             Console.WriteLine();
         }
     }
